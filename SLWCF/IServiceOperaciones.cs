d7c4b249abb2662ddb24a5107736615ea6b9234b
Leoesbravo <lescogidobravo@outlook.com>
2023-09-20 12:32:47 -0600

    WCF

---
+++ SLWCF/IServiceOperaciones.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+
+namespace SLWCF
+{
+     [ServiceContract]// que la interfaz es parte de un servicio WCF
+    public interface IServiceOperaciones
+    {
+        [OperationContract] //el metodo es parte del servicio WCF
+        void DoWork();
+        [OperationContract]
+        int Sumar(int a, int b);
+    }
+}
---
+++ SLWCF/ServiceOperaciones.svc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+
+namespace SLWCF
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "ServiceOperaciones" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select ServiceOperaciones.svc or ServiceOperaciones.svc.cs at the Solution Explorer and start debugging.
+    public class ServiceOperaciones : IServiceOperaciones
+    {
+        public void DoWork()
+        {
+        }
+        public int Sumar(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
