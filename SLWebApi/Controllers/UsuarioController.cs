0d7f05de4fa2fb25a36af1cd249c777ff3dcd0f1
Joserbp <josera.jack@gmail.com>
2023-09-22 16:28:23 -0600

    endpoint clean

--- SLWebApi/Controllers/UsuarioController.cs
+++ SLWebApi/Controllers/UsuarioController.cs
@@ -8,12 +8,46 @@ using System.Web.Routing;
 
 namespace SLWebApi.Controllers
 {
+    [RoutePrefix("api/usuario")]
     public class UsuarioController : ApiController
     {
-        [Route("api/usuario/add")]
+        [Route("")]
         [HttpPost]
         public IHttpActionResult Add(ML.Usuario usuario)
         {
+            ML.Result result = BL.Usuario.AddEF(usuario);
+            if (result.Correct)
+            {
+                return Content(HttpStatusCode.OK, result);
+            }
+            else
+            {
+                return Content(HttpStatusCode.BadRequest, result);
+            }
+        }
+
+        [Route("/{idUsuario}")]
+        [HttpDelete]
+        public IHttpActionResult Delete(int idUsuario)
+        {
+            ML.Result result = BL.Usuario.GetById(idUsuario);
+            if (result.Correct)
+            {
+                return Content(HttpStatusCode.OK, result);
+            }
+            else
+            {
+                return Content(HttpStatusCode.BadRequest, result);
+            }
+        }
+
+        [Route("/{idUsuario}")]
+        [HttpPut]
+        //Diferencia entre URL y URI
+        //Investigar metodos Asincronos y sincronos
+        public IHttpActionResult Update(int idUsuario, [FromBody] ML.Usuario usuario)
+        {
+            usuario.IdUsuario = idUsuario;
             ML.Result result = BL.Usuario.AddEF(usuario);
             if (result.Correct)
             {
