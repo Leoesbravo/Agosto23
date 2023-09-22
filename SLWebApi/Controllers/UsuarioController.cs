2d57b342ee9eff4645ba25b7804bed506ca5a9c9
Joserbp <josera.jack@gmail.com>
2023-09-21 18:05:36 -0600

    Add web api

---
+++ SLWebApi/Controllers/UsuarioController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Routing;
+
+namespace SLWebApi.Controllers
+{
+    public class UsuarioController : ApiController
+    {
+        [Route("api/usuario/add")]
+        [HttpPost]
+        public IHttpActionResult Add(ML.Usuario usuario)
+        {
+            ML.Result result = BL.Usuario.AddEF(usuario);
+            if (result.Correct)
+            {
+                return Content(HttpStatusCode.OK, result);
+            }
+            else
+            {
+                return Content(HttpStatusCode.BadRequest, result);
+            }
+        }
+    }
+}
